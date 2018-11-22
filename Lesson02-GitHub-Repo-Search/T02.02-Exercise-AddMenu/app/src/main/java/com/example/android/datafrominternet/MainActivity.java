4e358788d71131b4d3e188d460fa74186b01e38b
Roya Norouzi <royanorouzi@my.unt.edu>
2018-11-21 20:43:15 -0600

    Adding a menu to the java part.
    Here I faced a few problems, as the toDos suggested I tried to overwrite  onCreateOptionsMenu and then inflate it. To inflate it
    
    MenuInflater inflater = getMenuInflater();
    inflater.inflate(R.menu.main, menu);
    
    but the return super.onCreateOptionsMenu(menu) would cause error in my code so instead of removing it I made a try catch case.

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -17,6 +17,8 @@ package com.example.android.datafrominternet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -48,10 +50,28 @@ public class MainActivity extends AppCompatActivity {
     // TODO (7) Set the title to the search string ("Search") from strings.xml
     // Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        try {
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+
+        catch(Exception e){
+        return super.onCreateOptionsMenu(menu);}
+
+    }
+
 
     // TODO (8) Override onCreateOptionsMenu
     // TODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
     // TODO (10) Return true to display your menu
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
     // TODO (11) Override onOptionsItemSelected
     // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
