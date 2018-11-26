127aa5d8187464dea50e5799e69cfd741cc35bfe
Roya Norouzi <royanorouzi@my.unt.edu>
2018-11-26 14:30:10 -0600

    to get the ID of an item use item.getItemID and then use case for different possible item such as the code suggested here: https://developer.android.com/guide/topics/ui/menus To write and show a toast also use the following example to avoid any confusion. https://developer.android.com/guide/topics/ui/notifiers/toasts

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -15,12 +15,16 @@
  */
 package com.example.android.datafrominternet;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -70,7 +74,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     // TODO (11) Override onOptionsItemSelected
